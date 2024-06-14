720e784dc603d788f68e7cd1c5ebf766c58b3f9b
Bewis09 <bewis09minecraft@gmail.com>
2024-06-14 14:50:33 +0200

    Resource Pack downloader screen

---
+++ src/main/kotlin/bewis09/bewisclient/screen/AddResourcePackScreen.kt
@@ -0,0 +1,11 @@
+package bewis09.bewisclient.screen
+
+import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.text.Text
+
+class AddResourcePackScreen: Screen(Text.of("")) {
+    override fun render(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
+        super.render(context, mouseX, mouseY, delta)
+    }
+}
