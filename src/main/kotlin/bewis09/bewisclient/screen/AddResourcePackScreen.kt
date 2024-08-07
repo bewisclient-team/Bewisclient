8d068d4bd0ae46ec3837c21fdc4d83c30da73b29
Bewis09 <bewis09minecraft@gmail.com>
2024-08-07 17:13:50 +0200

    Buttons and only 20 on first page and lag fix

--- src/main/kotlin/bewis09/bewisclient/screen/AddResourcePackScreen.kt
+++ src/main/kotlin/bewis09/bewisclient/screen/AddResourcePackScreen.kt
@@ -2,12 +2,18 @@
 
 package bewis09.bewisclient.screen
 
+import bewis09.bewisclient.drawable.UsableTexturedButtonWidget
 import com.google.gson.Gson
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.SharedConstants
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.Drawable
+import net.minecraft.client.gui.Element
+import net.minecraft.client.gui.Selectable
+import net.minecraft.client.gui.screen.ButtonTextures
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.widget.ButtonWidget
 import net.minecraft.client.render.RenderLayer
 import net.minecraft.client.render.VertexConsumer
 import net.minecraft.client.sound.PositionedSoundInstance
@@ -33,6 +39,8 @@ import javax.imageio.ImageIO
 class AddResourcePackScreen(val parent: Screen): Screen(Text.of("")) {
     var scroll = 0.0
 
+    val drawables = arrayListOf<Drawable>()
+
     companion object {
         val gson = Gson()
 
@@ -43,20 +51,17 @@ class AddResourcePackScreen(val parent: Screen): Screen(Text.of("")) {
         val allHits = arrayListOf<ResourcePack>()
 
         init {
-            var total_hits = 1
-            var offset = 0
+            Thread { downloadWithOffset(0) }.start()
+        }
 
-            while (offset < total_hits) {
-                val connection =
-                    (URI("https://api.modrinth.com/v2/search?limit=100&offset=${offset}&facets=[[%22project_type:resourcepack%22],[%22versions:${SharedConstants.getGameVersion().name}%22]]").toURL()
-                        .openConnection()) as HttpURLConnection
-                val scanner = Scanner(connection.inputStream)
-                val rawRes = scanner.nextLine()
-                val response = gson.fromJson(rawRes, ResourcePackResponse::class.java)
-                offset+=100
-                total_hits = response.total_hits
-                allHits.addAll(response.hits)
-            }
+        fun downloadWithOffset(offset: Int) {
+            val connection =
+                (URI("https://api.modrinth.com/v2/search?limit=20&offset=${offset}&facets=[[%22project_type:resourcepack%22],[%22versions:${SharedConstants.getGameVersion().name}%22]]").toURL()
+                    .openConnection()) as HttpURLConnection
+            val scanner = Scanner(connection.inputStream)
+            val rawRes = scanner.nextLine()
+            val response = gson.fromJson(rawRes, ResourcePackResponse::class.java)
+            allHits.addAll(response.hits)
         }
     }
 
@@ -100,54 +105,95 @@ class AddResourcePackScreen(val parent: Screen): Screen(Text.of("")) {
     override fun render(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
         super.render(context, mouseX, mouseY, delta)
 
-        context?.fill((this.width/4)-2, 0, (this.width/4*3+2),this.height,
+        context?.fill(
+            (this.width / 4) - 2, 0, (this.width / 4 * 3 + 2), this.height,
             0xAA000000.toInt()
         )
 
-        fillGradient(context!!, (this.width/4) -8,0, ((this.width/4)) -2,this.height,0,
-            (0xAA000000).toInt()
+        fillGradient(
+            context!!, (this.width / 4) - 8, 0, ((this.width / 4)) - 2, this.height, 0,
+            (0xDD000000).toInt()
         )
 
-        fillGradient(context, (this.width-(this.width/4)) +2,0,this.width- (this.width/4) +8,this.height,
-            (0xAA000000).toInt(), 0)
+        fillGradient(
+            context, (this.width - (this.width / 4)) + 2, 0, this.width - (this.width / 4) + 8, this.height,
+            (0xDD000000).toInt(), 0
+        )
 
         var y = scroll.toInt()
 
-        allHits.forEachIndexed{_,a->
-            val hovered = mouseX>width/4&&mouseX<width/4*3&&y+2<mouseY&&y+38>mouseY
+        @Suppress("unchecked_cast")
+        (allHits.clone() as ArrayList<ResourcePack>).forEachIndexed { _, a ->
+            if (!(y + 2 > height || y + 38 < 0)) {
+                val hovered = mouseX > width / 4 && mouseX < width / 4 * 3 && y + 2 < mouseY && y + 38 > mouseY && mouseY<height-24
 
-            val description = textRenderer.wrapLines(StringVisitable.plain(a.description),width/2-60)
+                val description = textRenderer.wrapLines(StringVisitable.plain(a.description), width / 2 - 60)
 
-            context.matrices?.push()
-            if(hovered) {
-                context.matrices?.translate(width / 2f, y + 19f, 1f)
-                context.matrices?.scale((1 + 4f / width), (1 + 4f / width), 1f)
-                context.matrices?.translate(-width / 2f, -y - 19f, 1f)
-            }
+                context.matrices?.push()
+                if (hovered) {
+                    context.matrices?.translate(width / 2f, y + 19f, 1f)
+                    context.matrices?.scale((1 + 4f / width), (1 + 4f / width), 1f)
+                    context.matrices?.translate(-width / 2f, -y - 19f, 1f)
+                }
 
-            context.fill(width/4,y+2,width/4*3,y+38, 0xFF000000.toInt())
-            context.drawBorder(width/4,y+2,width/2,38, if(!hovered) -1 else 0xFFAAAAFF.toInt())
+                context.fill(width / 4, y + 2, width / 4 * 3, y + 38, 0xFF000000.toInt())
+                context.drawBorder(width / 4, y + 2, width / 2, 38, if (!hovered) -1 else 0xFFAAAAFF.toInt())
 
-            context.drawTextWithShadow(textRenderer,a.title,width/4+44,y+6,-1)
-            if(description.size!=0)
-                context.drawTextWithShadow(textRenderer,description[0],width/4+44,y+17,0xFF777777.toInt())
-            if(description.size>1)
-                context.drawTextWithShadow(textRenderer,OrderedText.concat(description[1],OrderedText.styledForwardsVisitedString(if(description.size>2) "..." else "",
-                    Style.EMPTY)),width/4+44,y+17+11,0xFF777777.toInt())
+                context.drawTextWithShadow(textRenderer, a.title, width / 4 + 44, y + 6, -1)
+                if (description.size != 0)
+                    context.drawTextWithShadow(textRenderer, description[0], width / 4 + 44, y + 17, 0xFF777777.toInt())
+                if (description.size > 1)
+                    context.drawTextWithShadow(
+                        textRenderer, OrderedText.concat(
+                            description[1], OrderedText.styledForwardsVisitedString(
+                                if (description.size > 2) "..." else "",
+                                Style.EMPTY
+                            )
+                        ), width / 4 + 44, y + 17 + 11, 0xFF777777.toInt()
+                    )
 
-            if(!(y+2>height || y+38<0)) {
-                if (!loadedImages.containsKey(a.icon_url) && loadedImages[a.icon_url] != LOADING_ID){
+                if (!loadedImages.containsKey(a.icon_url) && loadedImages[a.icon_url] != LOADING_ID) {
                     downloadImage(a.icon_url)
                 } else if (loadedImages[a.icon_url] != LOADING_ID) {
                     context.drawTexture(loadedImages[a.icon_url], width / 4 + 1, y + 3, 36, 36, 0f, 0f, 36, 36, 36, 36)
                 }
+
+                context.drawBorder(width / 4, y + 2, 38, 38, if (!hovered) -1 else 0xFFAAAAFF.toInt())
+
+                context.matrices?.pop()
             }
-            context.drawBorder(width/4,y+2,38,38,  if(!hovered) -1 else 0xFFAAAAFF.toInt())
 
-            y+=40
-
-            context.matrices?.pop()
+            y += 40
         }
+        context.matrices.push()
+        context.matrices.translate(0f,0f,5f)
+
+        context.fill(width/4-2,height-24,width/4*3+2,height, 0xDD000000.toInt())
+
+        drawables.forEach {
+            it.render(context, mouseX, mouseY, delta)
+        }
+
+        context.matrices.pop()
+    }
+
+    override fun init() {
+        scroll = MathHelper.clamp(scroll.toFloat(), -(allHits.size*40+2).toFloat()+height-24,0f).toDouble()
+
+        this.drawables.clear()
+
+        addDrawableChild(UsableTexturedButtonWidget(width/4,height-22,20,20, ButtonTextures(Identifier.of("bewisclient","textures/sprites/back_button.png"),Identifier.of("bewisclient","textures/sprites/back_button_highlighted.png"))){
+
+        })
+        addDrawableChild(UsableTexturedButtonWidget(width/4*3-20,height-22,20,20, ButtonTextures(Identifier.of("bewisclient","textures/sprites/next_button.png"),Identifier.of("bewisclient","textures/sprites/next_button_highlighted.png"))){
+
+        })
+    }
+
+    override fun <T> addDrawableChild(drawableElement: T): T where T : Element?, T : Drawable?, T : Selectable? {
+        drawables.add(drawableElement as Drawable)
+
+        return super.addDrawableChild(drawableElement)
     }
 
     private fun fillGradient(context: DrawContext, startX: Int, startY: Int, endX: Int, endY: Int, colorStart: Int, colorEnd: Int) {
@@ -228,7 +274,8 @@ class AddResourcePackScreen(val parent: Screen): Screen(Text.of("")) {
     ): Boolean {
         scroll += verticalAmount*20
 
-        scroll = MathHelper.clamp(scroll.toFloat(), -(allHits.size*40+2).toFloat()+height,0f).toDouble()
+        scroll = MathHelper.clamp(scroll.toFloat(), -(allHits.size*40+2).toFloat()+height-24,0f).toDouble()
+
         return super.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount)
     }
 
