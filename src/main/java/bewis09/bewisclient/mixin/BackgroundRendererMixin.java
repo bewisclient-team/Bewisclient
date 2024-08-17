5fcf6f30d9d6ab7aaaa4d9f998aed6243a7a043e
Bewis09 <bewis09minecraft@gmail.com>
2024-08-17 16:43:54 +0200

    Update to 1.21.1

--- src/main/java/bewis09/bewisclient/mixin/BackgroundRendererMixin.java
+++ src/main/java/bewis09/bewisclient/mixin/BackgroundRendererMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.block.enums.CameraSubmersionType;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
